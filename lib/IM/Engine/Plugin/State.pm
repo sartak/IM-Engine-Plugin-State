e94c15fb5813d5c9aa440e144bda61d030925c72
Shawn M Moore <sartak@gmail.com>
2009-08-08 23:01:38 -0400

    Skeleton

---
+++ lib/IM/Engine/Plugin/State.pm
@@ -0,0 +1,44 @@
+package IM::Engine::Plugin::State;
+use Moose;
+extends 'IM::Engine::Plugin';
+
+our $VERSION = '0.01';
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
+__END__
+
+=head1 NAME
+
+IM::Engine::Plugin::State - Keep track of some state for each user
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 AUTHOR
+
+Shawn M Moore, C<sartak@gmail.com>
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<IM::Engine>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Shawn M Moore.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
